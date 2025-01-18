2360bc1c4d3a60ea1fed4b9bd743f5d0178cde6e
Alexander <alexavi1712@gmail.com>
2025-01-18 15:49:22 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('#inicio');
+ScrollReveal().reveal('#skills', {delay : 500});
+ScrollReveal().reveal('#proyectos', {delay : 500});
+ScrollReveal().reveal('#contacto', {delay : 500});
+
